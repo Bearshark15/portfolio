9662ad96fe5aaf0b0bf7a00337993d7793939cfe
Jacob Bekele Jansson <jacob.bekelejansson.2014@student.uu.se>
2024-06-13 23:11:35 +0200

    updated text on the home page

--- components/ResumeEntryCard.tsx
+++ components/ResumeEntryCard.tsx
@@ -19,7 +19,7 @@ const ResumeEntryCard: React.FC<ResumeEntryCardProps> = ({ entry }) => {
                     </div>
                 </div>
                 <Subtitle className={"text-gray-400 font-normal"}>{entry.subtitle}</Subtitle>
-                <Paragraph className={"lg:w-1/2 w-full"}>{entry.description}</Paragraph>
+                <Paragraph className={"lg:w-1/2 w-full"} dangerouslySetInnerHTML={{__html: entry.description}}></Paragraph>
             </div>
         </section>
     )
